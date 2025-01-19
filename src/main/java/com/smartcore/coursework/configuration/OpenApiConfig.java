08e5c22db81da2f03c0fe84d86014e6bb91f0e85
vddmit <danvoropaeff@yandex.ru>
2025-01-19 22:21:39 +0300

    I configured Swagger, made the correct dependency, and wrote a configuration file.

---
+++ src/main/java/com/smartcore/coursework/configuration/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package com.smartcore.coursework.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Task Tracker API")
+                        .version("1.0.0")
+                        .description("API документация для Task Tracker")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
+
+}
---
+++ src/main/java/com/smartcore/coursework/configuration/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.smartcore.coursework.configuration;
+
+
+import com.smartcore.coursework.security.JwtTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtTokenRepository jwtTokenRepository;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf
+                        .csrfTokenRepository(csrfTokenRepository()))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**")
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .httpBasic(httpBasicConfigurer -> {
+                });
+        return http.build();
+    }
+
+    @Bean
+    public CsrfTokenRepository csrfTokenRepository() {
+        return jwtTokenRepository;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        return http.getSharedObject(AuthenticationManagerBuilder.class).build();
+    }
+}
