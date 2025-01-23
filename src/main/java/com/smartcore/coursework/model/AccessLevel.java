46266ae81b2f822d632829a2d52d76774e6aaa1e
vddmit <danvoropaeff@yandex.ru>
2025-01-23 12:59:00 +0300

    Added ordinal numbers.

--- src/main/java/com/smartcore/coursework/model/AccessLevel.java
+++ src/main/java/com/smartcore/coursework/model/AccessLevel.java
@@ -1,7 +1,7 @@
 package com.smartcore.coursework.model;
 
 public enum AccessLevel {
-    HIGH,
-    MEDIUM,
-    LOW
+    HIGH,   // ordinal() = 0
+    MEDIUM, // ordinal() = 1
+    LOW     // ordinal() = 2
 }
